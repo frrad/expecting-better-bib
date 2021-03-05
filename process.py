c01963a8c7dfc99b624eec43f74dd89bb706c2d4
Frederick Robinson <frederick.robinson@frrad.com>
2021-03-05 12:36:39 -0800

    basically works

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 import toml
+import crossref_commons.retrieval
 
 filename = "data.toml"
 
@@ -7,7 +8,24 @@ with open(filename, "r") as f:
 
 
 data = toml.loads(data_bytes)
-print(data)
+
+NAME = "name"
+DOI = "doi"
+STYLE = "apa"  # http://api.crossref.org/styles
+
+for chap in data:
+    for citation in data[chap]:
+        cite = data[chap][citation]
+        print(cite[DOI])
+
+        if cite[DOI] != "" and (NAME not in cite or cite[NAME] == ""):
+            metadata = crossref_commons.retrieval.get_publication_as_refstring(
+                cite[DOI], STYLE
+            )
+
+            metadata = metadata.strip()
+            cite[NAME] = metadata
+
 
 with open(filename, "w") as f:
     f.write(toml.dumps(data))
